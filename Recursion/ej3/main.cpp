619c97fb15916e3cbf73d34d8dd64d0a53ca74f4
agus <reyna.agustin2003@gmail.com>
2023-09-21 03:03:42 -0300

    git commit -m recursionnnn git push

---
+++ Recursion/ej3/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+ int sumaPares(int A[], int suma, int n){
+    if(n==0){
+        return suma;
+    }else{
+        if(A[n]%2==0){
+            suma=suma+A[n];
+        }
+        return sumaPares(A,suma,n-1);
+    }
+}
+
+int main() {
+    int N[100], tam, n;
+    int suma=0;
+    cout<<"Ingrese el tamanio del arreglo"<<endl;
+    cin>>tam;
+    for(int i=0; i<tam; i++){
+        cout<<"Ingrese el dato"<<endl;
+        cin>>N[i];
+    }
+    cout<<"Decime el n"<<endl;
+    do{
+        cin>>n;
+        if(n%2!=0){
+            cout<<"El numero ingresado es impar, no es valido"<<endl;
+        }
+    }while(n%2!=0)
+        ;
+    cout<<"La suma es "<<sumaPares(N,suma, n-1);
+    return 0;
+}
